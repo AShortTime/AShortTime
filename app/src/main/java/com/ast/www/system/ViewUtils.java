fac5967587592780c502ce48bfeaf6a8fb2403e1
gkq <872356736@qq.com>
2017-07-28 16:33:09 +0800

    11

---
+++ app/src/main/java/com/ast/www/system/ViewUtils.java
@@ -0,0 +1,70 @@
+package com.ast.www.system;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.GridView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+/**
+ * 作者:郭凯奇
+ * 时间: 2017/7/5 21:22
+ * Title:
+ * Text:
+ */
+
+public class ViewUtils {
+    public static void setListViewHeightBasedOnChildren(GridView gridView) {
+        // 获取listview的adapter
+        ListAdapter listAdapter = gridView.getAdapter();
+        if (listAdapter == null) {
+            return;
+        }
+        // 固定列宽，有多少列
+        int col = gridView.getNumColumns();
+        int totalHeight = 0;
+        // i每次加4，相当于listAdapter.getCount()小于等于4时 循环一次，计算一次item的高度，
+        // listAdapter.getCount()小于等于8时计算两次高度相加
+        for (int i = 0; i < listAdapter.getCount(); i += col) {
+            // 获取listview的每一个item
+            View listItem = listAdapter.getView(i, null, gridView);
+            listItem.measure(0, 0);
+            // 获取item的高度和
+            totalHeight += listItem.getMeasuredHeight();
+        }
+
+        // 获取listview的布局参数
+        ViewGroup.LayoutParams params = gridView.getLayoutParams();
+        // 设置高度
+        params.height = totalHeight;
+        // 设置margin
+//        ((ViewGroup.MarginLayoutParams) params).setMargins(10, 10, 10, 10);
+        // 设置参数
+        gridView.setLayoutParams(params);
+    }
+    public static void setListViewHeightBasedOnChildren(ListView listView) {
+
+        ListAdapter listAdapter = listView.getAdapter();
+
+        if (listAdapter == null) {
+            return;
+        }
+
+        int totalHeight = 0;
+
+        for (int i = 0; i < listAdapter.getCount(); i++) {
+            View listItem = listAdapter.getView(i, null, listView);
+            listItem.measure(0, 0);
+            totalHeight += listItem.getMeasuredHeight();
+        }
+
+        ViewGroup.LayoutParams params = listView.getLayoutParams();
+
+        params.height = totalHeight
+                + (listView.getDividerHeight() * (listAdapter.getCount() - 1));
+
+        ((ViewGroup.MarginLayoutParams) params).setMargins(10, 10, 10, 10); // 可删除
+
+        listView.setLayoutParams(params);
+    }
+}
